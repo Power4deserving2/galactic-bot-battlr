570fcc2ad8de0082aa3fe3a2f9fb440f9fd810a7
Immanuel <ipmaster40@gmail.com>
2025-05-05 12:53:07 +0300

    Fix bugs that did not export or import well

--- galactic-bot-battlr/src/App.js
+++ galactic-bot-battlr/src/App.js
@@ -14,6 +14,8 @@ function App() {
       .then((data) => setBots(data));
   }, []);
 
+  console.log("App component rendered");
+  
   return (
     <div className="App">
       <h1>Galactic Bot Battlr</h1>
---
+++ galactic-bot-battlr/src/components/YourBotArmy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function YourBotArmy({ army, setArmy }) {
+  return (
+    <div>
+      <h2>Your Bot Army</h2>
+      {/* You can render army bots here later */}
+    </div>
+  );
+}
+
+export default YourBotArmy;
