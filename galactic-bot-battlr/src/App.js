7cf8865af315a1c49de8873d04db5445c79bb8f1
Immanuel <ipmaster40@gmail.com>
2025-05-05 12:43:33 +0300

    Create basic file structure and setup App.js with state and layout

--- galactic-bot-battlr/src/App.js
+++ galactic-bot-battlr/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from "react";
+import BotCollection from "./components/BotCollection";
+import YourBotArmy from "./components/YourBotArmy";
+import "./App.css";
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/bots")
+      .then((res) => res.json())
+      .then((data) => setBots(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Galactic Bot Battlr</h1>
+      <YourBotArmy army={army} setArmy={setArmy} />
+      <BotCollection bots={bots} army={army} setArmy={setArmy} />
     </div>
   );
 }
