0597deef0081c7bd5081420bf49950a907d9c454
Immanuel <ipmaster40@gmail.com>
2025-05-05 12:53:56 +0300

    Add YourBotArmy component with remove functionality

--- galactic-bot-battlr/src/components/YourBotArmy.js
+++ galactic-bot-battlr/src/components/YourBotArmy.js
@@ -1,10 +1,19 @@
 import React from "react";
+import BotCard from "./BotCard";
 
 function YourBotArmy({ army, setArmy }) {
+  function handleRemove(bot) {
+    setArmy(army.filter((b) => b.id !== bot.id));
+  }
+
   return (
     <div>
       <h2>Your Bot Army</h2>
-      {/* You can render army bots here later */}
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {army.map((bot) => (
+          <BotCard key={bot.id} bot={bot} onClick={handleRemove} />
+        ))}
+      </div>
     </div>
   );
 }
